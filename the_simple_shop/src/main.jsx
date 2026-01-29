e96ee44601ae3cce23d61e72cba9bc1bebf4d42d
iptheprogrammer <ivan.polish01@gmail.com>
2026-01-29 13:43:30 +0100

    a very small change work proper work

--- the_simple_shop/src/main.jsx
+++ the_simple_shop/src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import App from "./App.jsx";
 import { theme } from "./theme";
-import "./index.css";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
